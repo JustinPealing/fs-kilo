4855af0c8fdcbf1654954b7119401ea0ccbd1a61
Justin Pealing <justin@pealing.me.uk>
2018-01-29 23:38:13 +0000

    Refactor processKeypress

--- Program.fs
+++ Program.fs
@@ -1,15 +1,13 @@
 ﻿open System
 
+let processKeypress (key:ConsoleKeyInfo) = 
+    if key.Modifiers.HasFlag(ConsoleModifiers.Control) && key.Key = ConsoleKey.Q then
+        exit 0
+
 let rec readloop() = 
     let key = Console.ReadKey true
-    if Char.IsControl key.KeyChar then
-        printfn "%d" (int key.KeyChar)
-    else
-        printfn "%d ('%c')" (int key.KeyChar) key.KeyChar
-    if key.Modifiers.HasFlag(ConsoleModifiers.Control) && key.Key = ConsoleKey.Q then
-        ()
-    else
-        readloop()
+    processKeypress key
+    readloop()
 
 [<EntryPoint>]
 let main argv =
