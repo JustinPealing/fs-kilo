fc2590c3aa164c007e3fafad0e7323f30487eca0
Justin Pealing <justin@pealing.me.uk>
2018-01-29 23:24:55 +0000

    Initial version

---
+++ Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let rec readloop() = 
+    let key = Console.ReadKey true
+    if Char.IsControl key.KeyChar then
+        printfn "%d" (int key.KeyChar)
+    else
+        printfn "%d ('%c')" (int key.KeyChar) key.KeyChar
+    readloop() 
+
+[<EntryPoint>]
+let main argv =
+    readloop()
+    0
