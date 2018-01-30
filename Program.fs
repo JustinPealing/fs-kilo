f34ec1da95cb84ae995181017c0bd4edf104250c
Justin Pealing <justin.pealing@audatex.co.uk>
2018-01-30 13:40:09 +0000

    Add welcome message

--- Program.fs
+++ Program.fs
@@ -1,23 +1,35 @@
 ﻿open System
 open System.Text
 
-let sb = new StringBuilder()
+type AppendBuffer = {
+    sb: StringBuilder
+}
+
+let abAppend ab (s:string) = 
+    ab.sb.Append(s.PadRight(Console.WindowWidth, ' ')) |> ignore
 
 let (|Ctrl|_|) k =
     if Char.IsControl k then Some (char ((int k) ||| 0x40))
     else None
 
-let editorDrawRows (sb:StringBuilder) =
+let editorDrawRows ab =
     for y in [0..Console.WindowHeight - 1] do
-        sb.Append ((sprintf "~").PadRight(Console.WindowWidth, ' ')) |> ignore
+        if (y = Console.WindowHeight / 3) then
+            let welcomeMessage = "FS-Kilo editor -- version 0.0.1"
+            if welcomeMessage.Length > Console.WindowWidth then
+                abAppend ab (welcomeMessage.Substring(0, Console.WindowWidth))
+            else
+                abAppend ab welcomeMessage
+        else
+            abAppend ab "~"
 
 let editorRefreshScreen() =
-    let sb = new StringBuilder()
-    editorDrawRows sb
+    let ab = {sb = new StringBuilder()}
+    editorDrawRows ab 
 
     Console.CursorVisible <- false
     Console.SetCursorPosition(0,0)
-    Console.Write(sb.ToString())
+    Console.Write(ab.sb.ToString())
     Console.SetCursorPosition(0,0)
     Console.CursorVisible <- true
 
