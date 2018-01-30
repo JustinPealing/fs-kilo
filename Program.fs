b9c8e3b8e73ede91fde7018913e1273822bc304c
Justin Pealing <justin@pealing.me.uk>
2018-01-30 00:40:14 +0000

    Draw ~

--- Program.fs
+++ Program.fs
@@ -4,13 +4,28 @@ let (|Ctrl|_|) k =
     if Char.IsControl k then Some (char ((int k) ||| 0x40))
     else None
 
+let resetScreen() =
+    Console.Clear()
+    Console.SetCursorPosition(0,0)
+
+let drawRows() =
+    for y in [0..23] do
+        printfn "~"
+
+let refreshScreen() =
+    resetScreen()
+    drawRows()
+    Console.SetCursorPosition(0,0)
+
 let processKeypress k =
     match k with
-    | Ctrl 'Q' -> exit 0
+    | Ctrl 'Q' ->
+        resetScreen()
+        exit 0
     | _ -> ()
 
 let rec readloop() = 
-    Console.Clear()
+    refreshScreen()
     let key = Console.ReadKey true
     processKeypress key.KeyChar
     readloop()
