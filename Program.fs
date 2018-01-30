27a8d4d59ac8bd0a75af1ca2f3014a22af2221fb
Justin Pealing <justin.pealing@audatex.co.uk>
2018-01-30 13:09:26 +0000

    Refactor to more closely match Kilo

--- Program.fs
+++ Program.fs
@@ -7,35 +7,34 @@ let (|Ctrl|_|) k =
     if Char.IsControl k then Some (char ((int k) ||| 0x40))
     else None
 
-let drawRows() =
-    sb.Clear() |> ignore
+let editorDrawRows (sb:StringBuilder) =
     for y in [0..Console.WindowHeight - 1] do
         sb.Append ((sprintf "~").PadRight(Console.WindowWidth, ' ')) |> ignore
-    sb.ToString()
 
-let refreshScreen() =
-    let rows = drawRows()
+let editorRefreshScreen() =
+    let sb = new StringBuilder()
+    editorDrawRows sb
+
     Console.CursorVisible <- false
     Console.SetCursorPosition(0,0)
-    Console.Write(rows)
+    Console.Write(sb.ToString())
     Console.SetCursorPosition(0,0)
     Console.CursorVisible <- true
 
-let processKeypress k =
-    match k with
+let editorProcessKeypress() =
+    let c = (Console.ReadKey true).KeyChar
+    match c with
     | Ctrl 'Q' ->
         Console.SetCursorPosition(0,0)
         Console.Clear()
         exit 0
     | _ -> ()
 
-let rec readloop() = 
-    refreshScreen()
-    let key = Console.ReadKey true
-    processKeypress key.KeyChar
-    readloop()
-
 [<EntryPoint>]
 let main argv =
+    let rec readloop() = 
+        editorRefreshScreen()
+        editorProcessKeypress()
+        readloop()
     readloop()
     0
